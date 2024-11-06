f7fc43f320105f7802bc855cc0512b006a5f55f7
MaxHolmes221B <maxholmes221b@gmail.com>
2024-11-06 22:41:16 +0700

    worst team sua chua loi lam

--- BACKEND/PickleballCourtBookingSystem/PickleballCourtBookingSystem.Core/Entities/Court.cs
+++ BACKEND/PickleballCourtBookingSystem/PickleballCourtBookingSystem.Core/Entities/Court.cs
@@ -6,6 +6,8 @@ namespace PickleballCourtBookingSystem.Api.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
+        public float Length { get; set; }
+        public float Width { get; set; }
         public Guid AddressId { get; set; }
         public Guid CourseOwnerId { get; set; }
     }
